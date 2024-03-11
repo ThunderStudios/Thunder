9efc5e16fac32ba82818b235306a6ca33f156ddf
Olle Lukowski <ollelukowski@hotmail.com>
2024-03-11 17:35:11 +0100

    Create thndr_core and thndr_ecs

---
+++ crates/thndr_core/src/lib.rs
@@ -0,0 +1,5 @@
+//! # thndr_core
+//!
+//! `thndr_core` is the core library for the Thunder Engine.
+//! It's main job is to re-export all the other libraries in the engine.
+//!
---
+++ crates/thndr_ecs/src/lib.rs
@@ -0,0 +1,5 @@
+//! # thndr_ecs
+//!
+//! `thndr_ecs` is a simple entity-component-system library for Rust, built for the Thunder Engine.
+//!
+//! Currently, it is a thin wrapper over [hecs](https://crates.io/crates/hecs), with some extra scheduling of systems.
